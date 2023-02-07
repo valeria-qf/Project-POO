ec02e7d9aaec8675c3821ebcf6b5595db3774f89
Valéria <vvqf2016@gmail.com>
2023-02-07 16:12:28 -0300

    docstrings adicionadas

--- Pokemon-Game/menu/pokemon_list/pokemon_list.py
+++ Pokemon-Game/menu/pokemon_list/pokemon_list.py
@@ -2,16 +2,20 @@ from pokemon_enum.pokemon_level_one import PokemonLevelOne
 from pokemon_enum.pokemon_level_two import PokemonLevelTwo
 from pokemon_enum.pokemon_level_three import PokemonLevelThree
 
+'''criação da classe PokemonList'''
 class PokemonList:
 
     def __init__(self) -> None:
         pass
-
+    
+    '''Método que quando chamado vai chamar a listagem de pokemon do level 1'''
     def list_pokemon_level1():
         PokemonLevelOne.print_values()
 
+    '''Método que quando chamado vai chamar a listagem de pokemon do level 2'''
     def list_pokemon_level2():
         PokemonLevelTwo.print_values()
 
+    '''Método que quando chamado vai chamar a listagem de pokemon do level 3'''
     def list_pokemon_level3():
         PokemonLevelThree.print_values()

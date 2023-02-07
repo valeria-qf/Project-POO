24b461f66ffc13e0b9c94449fd4aeaec9b5286c1
Valéria <vvqf2016@gmail.com>
2023-02-07 10:14:17 -0300

    documentação

--- Pokemon-Game/menu/pokedex.py
+++ Pokemon-Game/menu/pokedex.py
@@ -30,6 +30,7 @@ class Pokedex:
         os.system('cls')
 
     def select_pokemon_level(self):
+        # O start_game dá print no texto inicial do jogo
         self.start_game()
 
         print('\n[green]Selecione o nível dos Pokémon que irão batalhar:[white]\n\n1 - Nível 1 \n2 - Nível 2 \n3 - Nível 3\n')
@@ -87,6 +88,7 @@ class Pokedex:
         # arena.batalhar retorna o pokemon vencedor da batalha
         pokemon_vencedor = arena.batalhar()
 
+        # Se não houver vencedor, ou seja, empate, o hp dos pokemon será resetado, uma nova arena é instanciada e é chamada uma nova arena que também terá um vencedor como retorno. Essa conndição fica presa no while até existir um  retorno diferente de None
         if pokemon_vencedor == None:
                 while pokemon_vencedor == None:
                     pokemon_one.reset_hp()
@@ -96,28 +98,31 @@ class Pokedex:
 
         if pokemon_vencedor != None:
 
-        
+            # Quando houver um pokemon vencedor, o replay recebe True, logo vai ficar preso no while e verificando o vencedor e iniciando uma nova batalha
+
             replay: bool = True
             while replay:
 
                 pokemon_evolucao = pokemon_vencedor
 
+                # Checa se o vencedor é instância de cada pokémon e instancia um novo com a classe de evolução. Ex: se pokemon é instancia de Charmander, o pokemon_evolucao é sobrescrevido com a instância do Charmeleon(evolução de Charmander)
+
                 if isinstance(pokemon_evolucao, Charmander):
                     pokemon_evolucao = Charmeleon()
                 
-                elif isinstance(pokemon_vencedor, Charmeleon):
+                elif isinstance(pokemon_evolucao, Charmeleon):
                     pokemon_evolucao = Charizard()
 
-                elif isinstance(pokemon_vencedor,  Bubasaur):
+                elif isinstance(pokemon_evolucao,  Bubasaur):
                     pokemon_evolucao = Ivysaur()
 
-                elif isinstance(pokemon_vencedor, Ivysaur):
+                elif isinstance(pokemon_evolucao, Ivysaur):
                     pokemon_evolucao = Venusaur()
 
-                elif isinstance(pokemon_vencedor, Squirtle):
+                elif isinstance(pokemon_evolucao, Squirtle):
                     pokemon_evolucao = Wartortle()
 
-                elif isinstance(pokemon_vencedor, Wartortle):
+                elif isinstance(pokemon_evolucao, Wartortle):
                     pokemon_evolucao = Blastoise()
 
                 # Se o número de vitórias for menor que 3 os jogadores tem a opção de continuar, senão, ao batalhar no último nível, o número de vitórias é igual a 3 e jogo chega ao fim
@@ -126,12 +131,18 @@ class Pokedex:
                     print('[green]Deseja continuar? [bold][white]\n\n1 - SIM \n2 - NÃO\n')
                     acao = int(input())
 
+                    # Se o jogador decidir continuar jogando, é verificado o level do pokemon vencedor para quando o novo oponente for escolher outro pokemon para batalhar, terá que ser um do mesmo level do vencedor
+
                     if acao == 1:
                         nivel_vencedor = pokemon_vencedor.get_level()
 
                         print('[green]Escolha um Pokémon abaixo:\n')
 
+                        # o level 1 não é verificado, pois o vencedor sempre evolui após vencer a batalha, logo nunca será level 1
+
                         if nivel_vencedor == 2:
+                            # Se o vencedor for level 2, vai ser exibido ao novo oponente a lista de pokemon do level 2. Um input é recebido com a escolha do pokémon e é passado o level e a escolha do jogador nos parâmetros do método create pokemon, para ser criado um novo pokemon para o jogador que perdeu
+
                             self.__pokemon_list.list_pokemon_level2()
 
                             other_pokemon = int(input())
@@ -143,10 +154,13 @@ class Pokedex:
                             
                             other_pokemon = int(input())
                             new_oponent = self.create_pokemon(3, other_pokemon)
-                            
-                        pokemon_evolucao.reset_hp()
+                        
+                        # No fim da verificação, a nova arena é criada e é passado como parâmetro o pokemon vencedor(que evoluiu) e o novo oponente
+
                         new_arena = Arena(A = pokemon_evolucao, B = new_oponent)
+
                         #pokemon vencedor recebe new arena pois vai ser utilizado como retorno e renovar a variável pokemon evolução
+
                         pokemon_vencedor = new_arena.batalhar()
 
                         # Verifica se existiu empate após uma rodada de vitória
@@ -157,10 +171,12 @@ class Pokedex:
                                 newArena = Arena(A = pokemon_evolucao, B = new_oponent)
                                 pokemon_vencedor = newArena.batalhar(is_tie= True)
                         
+                    # Se o jogador dacidir não continuar a gameplay, replay recebe False e o jogo acaba
                     elif acao == 2:
                         replay = False
                         print('[bold][green]F I M')
 
+                # Número de batalhas maior que 2, os seja, já jogou a sua última batalha no level final, completando 3 vitórias
                 else:
                     print('[bold][green]F I M')
                     break
